d964b62ca258b0713b13f28688e35c7fc927914c
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2023-12-19 15:22:52 -0300

    headers middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,13 +2,17 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// page routes
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
-	return mux
+
+	// passed as next param to middleware
+	return secureHeaders(mux)
 }
