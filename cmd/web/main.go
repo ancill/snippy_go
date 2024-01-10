ab3d340638ad9ddacd577dffa8292e79e0565c77
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2024-01-10 18:58:08 -0300

    format

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -71,10 +71,13 @@ func main() {
 	}
 
 	srv := &http.Server{
-		Addr:      *addr,
-		ErrorLog:  errorLog,
-		Handler:   app.routes(),
-		TLSConfig: tlsConfig,
+		Addr:         *addr,
+		ErrorLog:     errorLog,
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		TLSConfig:    tlsConfig,
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
