8a0cee8b6f102dbc6a399fbf178730235bbb400f
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2024-02-13 12:31:35 -0300

    embedded files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
+	"snipper.ancill.com/ui"
 )
 
 func (app *application) routes() http.Handler {
@@ -15,8 +16,11 @@ func (app *application) routes() http.Handler {
 	})
 
 	// static files route
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	// Take the ui.Files embedded filesystem and convert it to a http.FS type so
+    // that it satisfies the http.FileSystem interface. We then pass that to the
+    // http.FileServer() function to create the file server handler.
+    fileServer := http.FileServer(http.FS(ui.Files))
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
 	// Unprotected application routes using the "dynamic" middleware chain.
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
