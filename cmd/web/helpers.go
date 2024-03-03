edfe451e94cb1f69e1df593080678cfe407898e4
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2024-03-03 20:25:59 -0300

    added debug mod

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -47,7 +47,13 @@ func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	app.errorLog.Output(2, trace)
 
+	if app.debug {
+		http.Error(w, trace, http.StatusInternalServerError)
+		return
+	}
+
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+
 }
 
 // Sends status code and description to user. Handle 400 statuses
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -26,11 +26,13 @@ type application struct {
 	templateCache  map[string]*template.Template
 	formDecoder    *form.Decoder
 	sessionManager *scs.SessionManager
+	debug          bool
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+	debug := flag.Bool("debug", false, "Debug mod")
 
 	flag.Parse()
 
@@ -66,6 +68,7 @@ func main() {
 		templateCache:  templateCache,
 		formDecoder:    formDecoder,
 		sessionManager: sessionManager,
+		debug:          *debug,
 	}
 
 	tlsConfig := &tls.Config{
